6ea6a61f1fe2d1eccfd40e5917ed2a2f996a4afc
Terra <terra.lindomar@gmail.com>
2025-01-10 13:31:49 -0300

    commit jan10 2025

--- src/layout/IntroComponent.jsx
+++ src/layout/IntroComponent.jsx
@@ -1,27 +1,12 @@
 import './IntroComponent.css'
-import Logo from '../images/logo_animation.png'
+
+import Logo from '../images/roseklamt_logo.png'
 
 const IntroComponent = () => {
   return (
     <div className="introComponent ">
-
-      <div >
-
-        <img src={Logo} alt="cozinhaAfetiva" />
-
-        <p className="suaChefAnimation">
-          <span className="textAnimation-1">Sua</span>
-          <span> </span>
-          <span className="textAnimation-2">Chef</span>
-          <span> </span>
-          <span className="textAnimation-3">à</span>
-          <span> </span>
-          <span className="textAnimation-4">Do</span>
-          <span className="textAnimation-5">mi</span>
-          <span className="textAnimation-6">cì</span>
-          <span className="textAnimation-7">lio</span>
-        </p>
-      </div>
+       
+       <img src={Logo} alt="roseklamt_logo" />        
     </div>
   )
 }
--- src/layout/NossosServicos.jsx
+++ src/layout/NossosServicos.jsx
@@ -3,43 +3,59 @@ import './NossosServicos.css'
 
 import { ImLink } from 'react-icons/im'
 
-
 const NossosServicos = () => {
   return (
-    <section className="NossoServicos_container mt-4 rounded-2 p-5 ">
-      <h2 className="text-center"> Nossos Serviços </h2>
-      <h4> Como funciona:</h4>
-      <p>
-        Chef à domicílio é um serviço exclusivo de culinária vegana
-        personalizada, que tem como intuíto facilitar o acesso de comida
-        saudável e saborosa para as pessoas que buscam qualidade de vida.
-      </p>
-      <p> Elaboramos e executamos um cardápio semanal para você e sua familia em sua própia casa de acordo com suas necesidades e preferências facilitando assim o seu dia-a-dia.  </p>
-      <p>
-        Nossa proposta foca em uma alimentação cem por cento
-        <strong> vegetariana estrita </strong> buscando trabalhar com
-        ingredientes frescos e produtos menos industrializados possível.
-      </p>
-
-      <h4>Outros Serviços:</h4>
-      <p>
-        Também oferecemos serviços de alimentação vegetariana estrita para eventos como 
-        catering, coffee-break e confraternização em geral. Elaboramos, preparamos, entregamos todo o menu em um local a escolha do cliente.
-      </p>
-      <p>
-        Nosso serviço de Catering é em parceria com a
-        <strong>
-          <Link
-            to="https://soulrebelrio.com/veganrioexperience"
-            target="_blank"
-          >
-            Soul Rebel Rio
-          </Link>
-          <ImLink />
-        </strong>
-        que fica no bairro Santa Teresa RJ, onde também oferecemos serviços de
-        <strong> Brunch </strong> semanal com agendamento prévio via whatsapp.
-      </p>
+    <section className="nossoServicos_container mt-4 rounded-2 p-4 p-lg-5 ">
+      <div className="animation_container">
+        <h2 className="p-4">Nossos Serviços </h2>
+        <h4 className="py-2 px-1"> Como funciona:</h4>
+        <p>
+          Chef à domicílio é um serviço exclusivo de culinária vegana
+          personalizada, que tem como intuíto facilitar o acesso de comida
+          saudável e saborosa para as pessoas que buscam qualidade de vida.
+        </p>
+        <p>
+          Elaboramos e executamos um cardápio semanal para você e sua familia em
+          sua própia casa de acordo com suas necesidades e preferências
+          <strong>facilitando</strong> assim o seu dia-a-dia.
+        </p>
+        <p>
+          Nossa proposta foca em uma alimentação cem por cento
+          <strong> vegetariana estrita </strong> buscando trabalhar com
+          ingredientes frescos e produtos menos industrializados possível.
+        </p>
+        <h4 className="py-2 px-1">Outros Serviços:</h4>
+        <p>
+          Também oferecemos serviços de alimentação vegetariana estrita para
+          eventos como catering, coffee-break e confraternização em geral.
+          Elaboramos, preparamos, entregamos todo o menu em um local a escolha
+          do cliente.
+        </p>
+        <p>
+          Nosso serviço de Catering é em parceria com a
+          <strong>
+            <Link
+              to="https://soulrebelrio.com/veganrioexperience"
+              target="_blank"
+            >
+              Soul Rebel Rio
+            </Link>
+            <ImLink />
+          </strong>
+          que fica no bairro Santa Teresa RJ, onde também oferecemos serviços de
+          <strong> Brunch </strong> semanal com agendamento prévio via{' '}
+          <strong>
+            <Link
+              to="https://soulrebelrio.com/veganrioexperience"
+              target="_blank"
+            >
+              Whatsapp.
+            </Link>
+            <ImLink />
+          </strong>
+          .
+        </p>
+      </div>
     </section>
   )
 }
