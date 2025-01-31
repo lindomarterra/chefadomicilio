9fe06060c0df5a3cedc0d015e72d1fb6cfbf425d
Terra <terra.lindomar@gmail.com>
2025-01-31 10:24:30 -0300

    commit jan 21 2025

--- src/layout/Menu/Catering.jsx
+++ src/layout/Menu/Catering.jsx
@@ -1,228 +1,154 @@
-import Catering01 from '../../images/catering/14.jpeg'
+import Catering01 from '../../images/catering/comidaButecoCatering.png'
 import Catering02 from '../../images/catering/12.jpeg'
 import Catering03 from '../../images/catering/4.jpeg'
+import { Link } from 'react-scroll'
 
 
-import GlutenFree from '../../images/icon/gluten.png'
 import './Cardapio.css'
+
 
 const Catering = () => {
   return (
     <section className="container bg-transparent py-5" id="catering">
-          <h5
-            className="py-4"
-            style={{
-              color: 'var(--color3)',
-              fontFamily: 'var(--font-4)',
-              textShadow: '2px 2px 2px #222',
-            }}
-          >
-            Sujestão de Catering:
-          </h5>
-    
-          {/* tema 01 */}
-          <div className="itemCardapio bg-light" >
-            <div className="row">
-              <div className="col-md-6">
-                <img
-                  src={Catering01}
-                  alt="Catering01"
-                  className="w-100 d-block rounded-end-5 img-thumbnail"
-                />
-              </div>
-              <div className="col-md-6">
-                <div className="text-black-50 my-3 ">
-                  <h5
-                    className="fst-italic text-center"
-                    style={{ fontFamily: 'var(--font-6)' }}
-                  >
-                    Brunches e cafés da manhã:
-                  </h5>
-                  <ul
-                    className="fst-italic"
-                    style={{ fontFamily: 'var(--font-6)' }}
-                  >
-                    <li>
-                      Tofu scrambled{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />
-                    </li>
-                    <li>
-                      {' '}
-                      Pão de polvilho{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />
-                    </li>
-                    <li>
-                      {' '}
-                      Homus{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                    <li>
-                      {' '}
-                      Caponata de berinjela{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                    <li>
-                      {' '}
-                      Pastinhas de leguminosas{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                    <li>
-                      {' '}
-                      Grãomelete{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                    <li>
-                      Pudding chia{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                    <li>Panquecas </li>
-                    <li>Torta salgada de legumes </li>
-                    <li>
-                      {' '}
-                      Leites vegetais e sucos naturais{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                    <li>
-                      Iaminho com granola{' '}
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{ width: '20px', paddingLeft: '5px' }}
-                      />{' '}
-                    </li>
-                  </ul>
-                </div>
-              </div>
+      <h5
+        className="py-4"
+        style={{
+          color: 'var(--color3)',
+          fontFamily: 'var(--font-4)',
+          textShadow: '2px 2px 2px #222',
+        }}
+      >
+        Sujestão de Catering em parceria com
+        <Link
+          style={{
+            color: 'var(--color3)',
+            fontFamily: 'var(--font-4)',
+            textShadow: '2px 2px 2px #222',
+            cursor:'pointer'
+          }}
+          to="https://soulrebelrio.com/veganrioexperience"
+          target="_blank"
+          className="ps-2 text-decoration-none"
+        >
+          @soulrebel.rio
+        </Link>
+        :
+      </h5>
+
+      {/* comida saudável */}
+      <div className="itemCardapio bg-light">
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src={Catering03}
+              alt="Catering03"
+              className="w-100 d-block rounded-end-5 img-thumbnail"
+            />
+          </div>
+          <div className="col-md-6">
+            <div className="text-black-50 my-3 ">
+              <h5
+                className="fst-italic text-center"
+                style={{ fontFamily: 'var(--font-6)' }}
+              >
+                Comida saudável:
+              </h5>
+              <ul
+                className="fst-italic"
+                style={{ fontFamily: 'var(--font-6)' }}
+              >
+                <li>Rolinhos de legumes com folha de arroz com molho satay</li>
+                <li>Bhuda bowl (arroz 7 grãos, tempeh, edamame...)</li>
+                <li> Arroz negro com brócoli, tempeh e couve-flor grelhados</li>
+                <li>
+                  Tofu ao molho de laranja com salada de ervilha torta e arroz
+                  oriental
+                </li>
+                <li>cheese cake crudivora com calda de frutas da estação</li>
+              </ul>
             </div>
           </div>
-          {/* tema 02 */}
-          <div className="itemCardapio bg-light mt-3 mt-md-0">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="text-black-50 mt-5">
-                  <h5
-                    className="fst-italic text-center"
-                    style={{ fontFamily: 'var(--font-6)' }}
-                  >
-                    Refeições:
-                  </h5>
-                  <ul
-                    className="fst-italic"
-                    style={{ fontFamily: 'var(--font-6)' }}
-                  >
-                    <li> Curry </li>
-                    <li> Mjadra</li>
-                    <li>Moqueca</li>
-                    <li>Feijoada</li>
-                    <li>Massas</li>
-                    <li>Burgers</li>
-                    <li>Lasanhas</li>
-                    <li>Pratos típicos reginonais </li>
-                    <li className="list-unstyled pt-2  ">
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{
-                          width: '20px',
-                          paddingRight: '5px',
-                          paddingBottom: '2px',
-                        }}
-                      />
-                      todos os pratos podem ser adaptados para glúten-free{' '}
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <img
-                  src={Catering02}
-                  alt="Catering02"
-                  className="w-100 d-block rounded-start-5 img-thumbnail "
-                />
-              </div>
+        </div>
+      </div>
+      {/* comidas regionais */}
+      <div className="itemCardapio bg-light mt-3 mt-md-0">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="text-black-50 mt-5">
+              <h5
+                className="fst-italic text-center"
+                style={{ fontFamily: 'var(--font-6)' }}
+              >
+                Comidas regionais brasileiras:
+              </h5>
+              <ul
+                className="fst-italic"
+                style={{ fontFamily: 'var(--font-6)' }}
+              >
+                <li>Palmito grelhado com molho pesto </li>
+                <li>Escondidinho </li>
+                <li>Moqueca</li>
+                <li>Feijoada</li>
+                <li>Banana da terra grelhada</li>
+                <li>Vinagrete de feijão fradinho</li>
+                <li>Acarajé</li>
+                <li>Carurú</li>
+                <li>Vatapá</li>
+                <li>Beijú com calda de baunilha</li>
+              </ul>
             </div>
           </div>
-          {/* tema 03 */}
-          <div
-            className="itemCardapio bg-light mt-3 mt-md-0" >
-            <div className="row">
-              <div className="col-md-6 ">
-                <img
-                  src={Catering03}
-                  alt="Catering03"
-                  className="w-100 d-block rounded-end-5 img-thumbnail "
-                />
-              </div>
-              <div className="col-md-6">
-                <div className="text-black-50 my-3 ">
-                  <h5
-                    className="fst-italic text-center"
-                    style={{ fontFamily: 'var(--font-6)' }}
-                  >
-                    Lanches, sobremesas:
-                  </h5>
-                  <ul
-                    className="fst-italic"
-                    style={{ fontFamily: 'var(--font-6)' }}
-                  >
-                    <li> Guacamole com tortilhas </li>
-                    <li>Focaccia</li>
-                    <li>Quiches</li>
-                    <li>Espeinhos de cogumelos com legumes</li>
-                    <li>Bolos</li>
-                    <li>Pavês</li>
-                    <li>Power balls</li>
-                    <li>Creme de abacate com cacau e granola</li>
-                    <li className="list-unstyled pt-2  ">
-                      <img
-                        src={GlutenFree}
-                        alt="gluten-free"
-                        style={{
-                          width: '20px',
-                          paddingRight: '5px',
-                          paddingBottom: '2px',
-                        }}
-                      />
-                      todos os pratos podem ser adaptados para glúten-free{' '}
-                    </li>
-                  </ul>
-                </div>
-              </div>
+          <div className="col-md-6">
+            <img
+              src={Catering02}
+              alt="Catering02"
+              className="w-100 d-block rounded-start-5 img-thumbnail "
+            />
+          </div>
+        </div>
+      </div>
+      {/* comida de buteco */}
+      <div className="itemCardapio bg-light mt-3 mt-md-0">
+        <div className="row">
+          <div className="col-md-6 ">
+            <img
+              src={Catering01}
+              alt="Catering01"
+              className="w-100 d-block rounded-end-5 img-thumbnail "
+            />
+          </div>
+          <div className="col-md-6">
+            <div className="text-black-50 my-3 ">
+              <h5
+                className="fst-italic text-center"
+                style={{ fontFamily: 'var(--font-6)' }}
+              >
+                Comida de buteco e finger food:
+              </h5>
+              <ul
+                className="fst-italic"
+                style={{ fontFamily: 'var(--font-6)' }}
+              >
+                <li>Guacamole com chips </li>
+                <li>Coinha de shimeji</li>
+                <li>Bolinho de feijoada com geléia de pimenta</li>
+                <li>
+                  Batata recheada com queijo cremoso de castanha e espinafre
+                </li>
+                <li>Pasteis com vinagrete</li>
+                <li>Cubos de tempeh marinados e grelhados</li>
+                <li>Espetinhos de cogumelos com legumes e farofa </li>
+                <li>
+                  {' '}
+                  Barquetes com queijo cremoso de castanha e cebola caramelizada{' '}
+                </li>
+                <li>Trufa de chocolate com laranja</li>
+              </ul>
             </div>
           </div>
-        </section>
+        </div>
+      </div>
+    </section>
   )
 }
 
