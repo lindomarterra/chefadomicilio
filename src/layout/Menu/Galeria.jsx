9a7fbd70f27533f32fec6f1c649a6c857d8190f6
Terra <terra.lindomar@gmail.com>
2025-02-05 21:09:39 -0300

    commit feb 5 2025

--- src/layout/Menu/Galeria.jsx
+++ src/layout/Menu/Galeria.jsx
@@ -36,7 +36,7 @@ const Gallery = () => {
         {/** line one */}
         <div className="row g-0 lineAnime">
           <div className="col-md-6 col-lg-4  ">
-            <figure className="position-relative d-flex justify-content-center">
+            <figure className="position-relative d-flex justify-content-center m-0">
               <img
                 style={{ height: '250px' }}
                 src={Refeicao2}
@@ -53,136 +53,256 @@ const Gallery = () => {
                 Refeições para semanas em marmitas congeladas
               </figcaption>
             </figure>
-
-            <span>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '250px' }}
                 src={Refeicao1}
                 alt="refeicao1"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '500px' }}
                 src={Refeicao3}
                 alt="refeicao3"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center m-0">
               <img
                 style={{ height: '250px' }}
                 src={Refeicao5}
                 alt="refeicao5"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
-            <span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '250px' }}
                 src={Refeicao4}
                 alt="refeicao4"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
         </div>
 
         {/** line two  */}
         <div className="row g-0 lineAnime">
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '500px' }}
                 src={Brunch3}
                 alt="brunch3"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
+
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center m-0">
               <img
                 style={{ height: '250px' }}
                 src={Brunch1}
                 alt="brunch1"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
 
-            <span>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '250px' }}
                 src={Brunch2}
                 alt="brunch2"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '500px' }}
                 src={Brunch4}
                 alt="brunch4"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
+            <span></span>
           </div>
         </div>
 
         {/** line three */}
         <div className="row g-0 lineAnime">
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '500px' }}
                 src={Petisco1}
                 alt="petisco1"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
           <div className="col-md-6 col-lg-4  ">
-            <span>
+            <figure className="position-relative d-flex justify-content-center m-0">
               <img
                 style={{ height: '250px' }}
                 src={Petisco2}
                 alt="petisco2"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
-            <span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
+            <figure className="position-relative d-flex justify-content-center">
               <img
                 style={{ height: '250px' }}
                 src={Petisco3}
                 alt="petisco3"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
           </div>
           <div className="col-md-6 col-lg-4">
-            <span>
+            <figure className="position-relative d-flex justify-content-center m-0">
               <img
                 style={{ height: '300px' }}
                 src={Petisco6}
                 alt="petisco6"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
-            <span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
+
+            <figure className="position-relative d-flex justify-content-center m-0">              
               <img
                 style={{ height: '200px' }}
                 src={Petisco7}
                 alt="petisco7"
                 className="d-block w-100 rounded-4 p-2"
               />
-            </span>
+              <figcaption
+                style={{
+                  fontFamily: 'var(--font-6)',
+                  backgroundColor: '#6cad63',
+                }}
+                className="position-absolute bottom-0 mb-3 z-3 text-white w-75 text-center fw-bolder rounded-1"
+              >
+                Refeições para semanas em marmitas congeladas
+              </figcaption>
+            </figure>
+
           </div>
         </div>
       </div>
