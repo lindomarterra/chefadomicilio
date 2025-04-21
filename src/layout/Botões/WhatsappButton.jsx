916e0955351bcc0fc9865a991778fb6ac89a3340
Terra <terra.lindomar@gmail.com>
2025-04-21 17:17:24 -0300

    whatsapp changes

--- src/layout/Botões/WhatsappButton.jsx
+++ src/layout/Botões/WhatsappButton.jsx
@@ -6,7 +6,7 @@ const WhatsappButton = () => {
   return (
     <Link
       className="btn_whats"
-      to="https://wa.me//555196944233?text=Olá! Hello Rosy! How are you? I´m interested in your job."
+      to="https://wa.me//555196944233?text=Hello Rosy! How are you? I am interested in your job."
       alt='Entrar em contato pelo whatsapp'
       target="_blank"
       title="Speak with Rosy Klamt"
